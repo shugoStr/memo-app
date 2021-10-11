ac3515f3e612da2f10d72ea5c63358ead3f1a187
shugowork <schw4one1win@gmail.com>
2021-10-11 22:46:47 +0900

    Implement up to the new.erb
    index.erb to new.erb

--- app.rb
+++ app.rb
@@ -3,10 +3,23 @@ require 'sinatra/reloader'
 require 'erb'
 require 'json'
 
+
 get '/' do
   redirect to('/memos')
 end
 
 get '/memos' do
   erb :index
+end
+
+get '/new' do
+  erb :new
+end
+
+get '/crate' do
+  redirect to('/memos')
+end
+
+not_found do
+  "ops! the requested information wasn't available."
 end
