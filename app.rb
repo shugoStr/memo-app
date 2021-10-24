8b82cf490f360ec3befbd01b782ddee036c2968c
ShuGo <schw4one1win@gmail.com>
2021-10-24 22:58:52 +0900

    tmp

--- app.rb
+++ app.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
-require 'sinatra'
-require 'sinatra/reloader'
-require 'erb'
-require 'sinatra/json'
+require "sinatra"
+require "sinatra/reloader"
+require "erb"
+require "sinatra/json"
+require "pg"
+
+memo = Memo.new(memo)
 
 # XSS
 helpers do
@@ -12,57 +15,95 @@ helpers do
   end
 end
 
-get '/' do
-  redirect to('/memos')
+get "/" do
+  redirect to("/memos")
 end
 
 def build_json_data
-  Dir.glob('storage/*').sort.map { |file| JSON.parse(File.read(file)) }
+  Dir.glob("storage/*").sort.map { |file| JSON.parse(File.read(file)) }
 end
 
-get '/memos' do
-  @memos = build_json_data
+get "/memos" do
+  # @memos = build_json_data
+
   erb :index
 end
 
-get '/new' do
+get "/new" do
   erb :new
 end
 
-post '/memos' do
-  params[:id] = Time.now.strftime('%Y%m%d_%H%M%S')
+post "/memos" do
+  params[:id] = Time.now.strftime("%Y%m%d_%H%M%S")
   data_hash = { id: params[:id], title: params[:title], content: params[:content] }
-  File.open("storage/#{Time.now.strftime('%Y%m%d_%H%M%S')}.json", 'w') { |file| file.puts JSON.generate(data_hash) }
-  redirect to('/memos')
+  File.open("storage/#{Time.now.strftime("%Y%m%d_%H%M%S")}.json", "w") { |file| file.puts JSON.generate(data_hash) }
+  redirect to("/memos")
 end
 
 def file_path
   "storage/#{File.basename(params[:id])}.json"
 end
 
-get '/memos/:id' do
+get "/memos/:id" do
   @memo = JSON.parse(File.read(file_path), symbolize_names: true)
   erb :show
 end
 
-get '/memos/:id/edit' do
+get "/memos/:id/edit" do
   @memo = JSON.parse(File.read(file_path), symbolize_names: true)
   erb :edit
 end
 
-patch '/memos/:id' do
-  File.open(file_path, 'w') do |file|
+patch "/memos/:id" do
+  File.open(file_path, "w") do |file|
     data_hash = { id: params[:id], title: params[:title], content: params[:content] }
     JSON.dump(data_hash, file)
   end
   redirect to("/memos/#{params[:id]}")
 end
 
-delete '/memos/:id' do
+delete "/memos/:id" do
   File.delete(file_path)
-  redirect to('/memos')
+  redirect to("/memos")
 end
 
 not_found do
   erb :not_found
+end
+
+class Memo
+  attr_accessor :memo_data
+
+  def initialize(dbname)
+    @connection = PG.connect(dbname: "dbname")
+    @memo_data = memo.reload_memo
+    connection = PG::Connection.new[:dbname]
+  end
+
+  def reload
+
+  end
+
+  def find(id)
+
+  end
+
+  def add
+    # connection.exec("INSERT INTO inventory VALUES(1, 'banana', 150)")
+  end
+
+  def update
+
+  end
+
+  def delete
+
+  end
+
+rescue PG::Error => e
+  puts e.message
+ensure
+  connection.close if connection
+end
+
 end
