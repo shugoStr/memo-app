6176735f1b02fd3a56a8d654d9d9743909f2715e
ShuGo <schw4one1win@gmail.com>
2021-10-28 22:37:56 +0900

    Implement index and new
    First commit on DB branch

--- app.rb
+++ app.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
-require 'sinatra'
-require 'sinatra/reloader'
-require 'erb'
-require 'sinatra/json'
+require "sinatra"
+require "sinatra/reloader"
+require "erb"
+require "pg"
+
+connect = PG.connect(dbname: "memo")
 
 # XSS
 helpers do
@@ -12,55 +14,40 @@ helpers do
   end
 end
 
-get '/' do
-  redirect to('/memos')
+get "/" do
+  redirect to("/memos")
 end
 
-def build_json_data
-  Dir.glob('storage/*').sort.map { |file| JSON.parse(File.read(file)) }
-end
-
-get '/memos' do
-  @memos = build_json_data
+get "/memos" do
+  @memos = connect.exec("SELECT * FROM memos;")
   erb :index
 end
 
-get '/new' do
+get "/new" do
   erb :new
 end
 
-post '/memos' do
-  params[:id] = Time.now.strftime('%Y%m%d_%H%M%S')
-  data_hash = { id: params[:id], title: params[:title], content: params[:content] }
-  File.open("storage/#{Time.now.strftime('%Y%m%d_%H%M%S')}.json", 'w') { |file| file.puts JSON.generate(data_hash) }
-  redirect to('/memos')
+post "/memos" do
+  title = params[:title]
+  content = params[:content]
+  connect.exec("INSERT INTO memos (title, content) VALUES ($1, $2);", [title, content])
+  redirect to("/memos")
 end
 
-def file_path
-  "storage/#{File.basename(params[:id])}.json"
-end
-
-get '/memos/:id' do
-  @memo = JSON.parse(File.read(file_path), symbolize_names: true)
+get "/memos/:id" do
   erb :show
 end
 
-get '/memos/:id/edit' do
-  @memo = JSON.parse(File.read(file_path), symbolize_names: true)
+get "/memos/:id/edit" do
   erb :edit
 end
 
-patch '/memos/:id' do
-  File.open(file_path, 'w') do |file|
-    data_hash = { id: params[:id], title: params[:title], content: params[:content] }
-    JSON.dump(data_hash, file)
-  end
+patch "/memos/:id" do
   redirect to("/memos/#{params[:id]}")
 end
 
-delete '/memos/:id' do
-  File.delete(file_path)
-  redirect to('/memos')
+delete "/memos/:id" do
+  redirect to("/memos")
 end
 
 not_found do
