4fed4adda30b519b02e4b19b12848d43b86a36a2
ShuGo <schw4one1win@gmail.com>
2021-11-06 13:53:27 +0900

    fix rubocop
    Prefer to_s over string interpolation.

--- app.rb
+++ app.rb
@@ -29,27 +29,35 @@ get '/new' do
 end
 
 post '/memos' do
-  connect.exec('INSERT INTO memos (title, content) VALUES ($1, $2)', ["#{params[:title]}", "#{params[:content]}"])
+  title = params[:title]
+  content = params[:content]
+  connect.exec('INSERT INTO memos (title, content) VALUES ($1, $2)', [title, content])
   redirect to('/memos')
 end
 
 get '/memos/:id' do
-  @memo = connect.exec('SELECT * FROM memos WHERE id = $1', ["#{params[:id]}"])
+  id = params[:id]
+  @memo = connect.exec('SELECT * FROM memos WHERE id = $1', [id])
   erb :show
 end
 
 get '/memos/:id/edit' do
-  @memo = connect.exec('SELECT * FROM memos WHERE id = $1', ["#{params[:id]}"])
+  id = params[:id]
+  @memo = connect.exec('SELECT * FROM memos WHERE id = $1', [id])
   erb :edit
 end
 
 patch '/memos/:id' do
-  connect.exec('UPDATE memos SET title = $1, content = $2 WHERE id = $3', ["#{params[:title]}", "#{params[:content]}","#{params[:id]}"])
+  title = params[:title]
+  content = params[:content]
+  id = params[:id]
+  connect.exec('UPDATE memos SET title = $1, content = $2 WHERE id = $3', [title, content, id])
   redirect to("/memos/#{params[:id]}")
 end
 
 delete '/memos/:id' do
-  connect.exec('DELETE FROM memos WHERE id = $1', ["#{params[:id]}"])
+  id = params[:id]
+  connect.exec('DELETE FROM memos WHERE id = $1', [id])
   redirect to('/memos')
 end
 
